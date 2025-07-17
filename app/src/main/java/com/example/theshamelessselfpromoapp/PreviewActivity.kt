5dc1558345573dd8afa8478bd55ff5d1a2b06100
Alan Amalemba <alan.amalemba@lctafrica.net>
2025-07-17 12:49:02 +0300

    display message from seralized object of Message data class

--- app/src/main/java/com/example/theshamelessselfpromoapp/PreviewActivity.kt
+++ app/src/main/java/com/example/theshamelessselfpromoapp/PreviewActivity.kt
@@ -1,35 +1,37 @@
 package com.example.theshamelessselfpromoapp
 
+import android.os.Build
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class PreviewActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preview)
 
-        val contactName = intent.getStringExtra("Contact Name")
-        val contactNumber = intent.getStringExtra("Contact Number")
-        val displayName = intent.getStringExtra("Display Name")
-        val includeJunior = intent.getBooleanExtra("Include Junior",false)
-        val jobTitle = intent.getStringExtra("Job Title")
-        val immediateStart = intent.getBooleanExtra("Immediate Start",false)
-        val startDate = intent.getStringExtra("Start Date")
+        val message = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent.getSerializableExtra("message", Message::class.java)
+        } else {
+            @Suppress("DEPRECATION")
+            intent.getSerializableExtra("message") as? Message
+        }
 
-        val testString = "Contact Name: $contactName, " +
-                "Contact Number: $contactNumber, " +
-                "Display Name: $displayName, " +
-                "Include Junior: $includeJunior, " +
-                "Job Title: $jobTitle, " +
-                "Immediate Start: $immediateStart, " +
-                "Start Date: $startDate"
+        val messagePreviewText = """
+         ${message?.contactName}, 
+         My name is ${message?.myDisplayName} and I am ${message?.getFullJobDescription()}.
+         I hava a portfolio of apps to demonstrate my skill.
+         I am able to start a new position ${message?.getAvailability()}
+         Please get in touch.
+         Thanks and best regards
+     """.trimIndent()
 
-        val messageTextView: TextView = findViewById(R.id.text_view_message)
-
-        messageTextView.text = testString
+        val messageTextView:TextView = findViewById(R.id.text_view_message)
+        messageTextView.text = messagePreviewText
     }
 }
