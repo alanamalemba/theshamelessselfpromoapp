71d4490ab2c822cfa8db372f1b7371bc4c9791bb
Alan Amalemba <alan.amalemba@lctafrica.net>
2025-07-22 08:45:18 +0300

    feat: implememt sending of a text message

--- app/src/main/java/com/example/theshamelessselfpromoapp/PreviewActivity.kt
+++ app/src/main/java/com/example/theshamelessselfpromoapp/PreviewActivity.kt
@@ -1,7 +1,10 @@
 package com.example.theshamelessselfpromoapp
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
@@ -10,28 +13,53 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class PreviewActivity : AppCompatActivity() {
+
+    private lateinit var message: Message
+    private lateinit var messagePreviewText: String
+
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preview)
 
-        val message = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        displayMessage()
+        setupButton()
+    }
+
+    private fun displayMessage() {
+        message = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             intent.getSerializableExtra("message", Message::class.java)
+                ?: throw IllegalArgumentException("Message is missing in intent extras")
         } else {
             @Suppress("DEPRECATION")
-            intent.getSerializableExtra("message") as? Message
+            (intent.getSerializableExtra("message") as? Message)
+                ?: throw IllegalArgumentException("Message is missing in intent extras")
         }
 
-        val messagePreviewText = """
-         ${message?.contactName}, 
-         My name is ${message?.myDisplayName} and I am ${message?.getFullJobDescription()}.
-         I hava a portfolio of apps to demonstrate my skill.
-         I am able to start a new position ${message?.getAvailability()}
-         Please get in touch.
-         Thanks and best regards
-     """.trimIndent()
 
-        val messageTextView:TextView = findViewById(R.id.text_view_message)
+        messagePreviewText = """
+             ${message?.contactName}, 
+             My name is ${message?.myDisplayName} and I am ${message?.getFullJobDescription()}.
+             I hava a portfolio of apps to demonstrate my skill.
+             I am able to start a new position ${message?.getAvailability()}
+             Please get in touch.
+             Thanks and best regards
+         """.trimIndent()
+
+        val messageTextView: TextView = findViewById(R.id.text_view_message)
         messageTextView.text = messagePreviewText
+    }
+
+    private fun setupButton() {
+        val sendMessageButton: Button = findViewById(R.id.button_send_message)
+
+        sendMessageButton.setOnClickListener{
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("smsto: ${message.contactNumber}") // ensures only sms apps respond
+                putExtra("sms_body", messagePreviewText)
+            }
+            startActivity(intent)
+        }
+
     }
 }
